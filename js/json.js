c7c5eb11630bd3b0b06624fd66cd36b2afdde613
Josephss <Josephsolomon2010@gmail.com>
2015-04-01 14:56:03 -0500

    4-1-2015

--- js/json.js
+++ js/json.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
 	//$.getJSON("http://osmonds.usd.edu/BTL-REST/resources/tools/", function(data) {
    			// alert(data);
    		//$.each(data, function(i, f) {
-		// var tool_div_row = "<div id='two-columns parent' class='grid-container centered' style='display:block;'>" + "<ul class='col-md-3'>" + "<li style='list-style-type: none;' class = 'bor' >" + "<img id = 'pop' src='images/tools-icon.jpg' class='img-responsive center-block lefter'>" + "</img>" + "<h3>" + f.name + "</h3>" + "<p>" + f.version + "</p>"  +  "<p class = 'discriptionPad' >" + f.description + "</p>" + "<div class = 'smile'>" + "<a href='http://google.com'>" + "<img class = 'ver' src='img/play.png' />" + "</a>" + "<a href='http://google.com'>" + "<img class = 'ver' src='img/eye.png' align='center''/>" + "</a> " + "<a href='http://google.com'>" +"<img  class = 'ver' src='img/quality.png' align='center''/>" + "</a>" + "<a href='http://google.com'>" + "<img src='img/support.png' align='center''/>" + "</a>"  + "</div>" + "</li>" + "</ul>" + "</div>"
-		 //+  "<a href='http://google.com'>" + "<img src='img/docker-whale.png' align='center''/>" + "</a>"  -- Docker icon!
+		// var tool_div_row = "<div id='two-columns parent' class='grid-container centered' style='display:block;'>" + "<ul class='col-md-3'>" + "<li style='list-style-type: none;' class = 'bor' >" + "<img id = 'pop' src='images/tools-icon.jpg' class='img-responsive center-block lefter'>" + "</img>" + "<h3>" + f.name + "</h3>" + "<p>" + f.version + "</p>"  +  "<p class = 'discriptionPad' >" + f.description + "</p>" + "<div class = 'smile'>" + "<a href='#'>" + "<img class = 'ver' src='img/play.png' />" + "</a>" + "<a href='#'>" + "<img class = 'ver' src='img/eye.png' align='center''/>" + "</a> " + "<a href='#'>" +"<img  class = 'ver' src='img/quality.png' align='center''/>" + "</a>" + "<a href='#'>" + "<img src='img/support.png' align='center''/>" + "</a>"  + "</div>" + "</li>" + "</ul>" + "</div>"
+		 //+  "<a href='#'>" + "<img src='img/docker-whale.png' align='center''/>" + "</a>"  -- Docker icon!
 
      //   $(tool_div_row).appendTo("#three-columns");
         	//});
@@ -29,10 +29,10 @@ $(document).ready(function(){
       
      });
 
-       var tool_icons = "<div id='righterr' class='grid-container' style='display:block;'>" + "<ul class='col-md-3'>" + "<li style='list-style-type: none;' class = 'bor' >" +  "<div class = 'smile'>" +  "<a href='#'>" + "<img src='img/arrow_up_1.png' align='center''/>" + "</a>" + "<a href='http://google.com'>" + "<img class = 'ver' src='img/play.png' />" + "</a>" + "<a href='http://google.com'>" + "<img class = 'ver' src='img/eye.png' align='center''/>" + "</a> " + "<a href='http://google.com'>" +"<img  class = 'ver' src='img/quality.png' align='center''/>" + "</a>" + "<a href='http://google.com'>" + "<img src='img/support.png' align='center''/>" + "</a>" +  "<a href='http://google.com'>" + "<img src='img/docker-whale.png' align='center''/>" + "</a>"  +  "<a href='#'>" + "<img src='img/arrow_down_2.png' align='center''/>" + "</a>" + "</div>" + "</li>" + "</ul>" + "</div>"
-		 //+  "<a href='http://google.com'>" + "<img src='img/docker-whale.png' align='center''/>" + "</a>"  -- Docker icon!
-		  //+  "<a href='http://google.com'>" + "<img src='img/arrow_down_2.png' align='center''/>" + "</a>"  -- up icon!
-		   //+  "<a href='http://google.com'>" + "<img src='img/arrow_up_1.png' align='center''/>" + "</a>"  -- down icon!
+       var tool_icons = "<div id='righterr' class='containerr' align='right'>" + "<ul class='col-md-3'>" + "<li style='list-style-type: none;' class = 'bor' >" +  "<div class = 'righterr '>" +  "<a href='#'>" + "<img src='img/arrow_up_1.png' align='center''/>" + "</a>" + "<a href='#'>" + "<img class = 'ver' src='img/play.png' />" + "</a>" + "<a href='#'>" + "<img class = 'ver' src='img/eye.png' align='center''/>" + "</a> " + "<a href='#'>" +"<img  class = 'ver' src='img/quality.png' align='center''/>" + "</a>" + "<a href='#'>" + "<img src='img/support.png' align='center''/>" + "</a>" +  "<a href='#'>" + "<img src='img/docker-whale.png' align='center''/>" + "</a>"  +  "<a href='#'>" + "<img src='img/arrow_down_2.png' align='center''/>" + "</a>" + "</div>" + "</li>" + "</ul>" + "</div>"
+		 //+  "<a href='#'>" + "<img src='img/docker-whale.png' align='center''/>" + "</a>"  -- Docker icon!
+		  //+  "<a href='#'>" + "<img src='img/arrow_down_2.png' align='center''/>" + "</a>"  -- up icon!
+		   //+  "<a href='#'>" + "<img src='img/arrow_up_1.png' align='center''/>" + "</a>"  -- down icon!
 		 // + "<a class='resize' href='f.summary'>"+ 'summary' + "</a>" 
 		 //+ "<p>" + f.version + "</p>"  +  "<p class = 'discriptionPad' >" + f.description + "</p>"
 		   $(tool_icons).appendTo("#righterr");
